f46baa5e32a162fabb5ba2bcde2fa19539584401
Guilherme Rocha <GuiPython2001@gmail.com>
2022-09-07 23:10:31 -0300

    config
    .gitignore
    jest.config
    tsconfig
    jest 26.0.0

--- jest.config.ts
+++ jest.config.ts
@@ -71,7 +71,7 @@ export default {
   // globals: {},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
-  // maxWorkers: "50%",
+  maxWorkers: "4",
 
   // An array of directory names to be searched recursively up from the requiring module's location
   // moduleDirectories: [
@@ -188,7 +188,7 @@ export default {
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
+  verbose: false,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
